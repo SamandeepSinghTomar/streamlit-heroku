68bbabe09328c4dd451d1029867617a1db305d74
Samandeep Singh Tomar <50411635+SamandeepSinghTomar@users.noreply.github.com>
2022-07-24 22:00:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+import pickle
+
+st.write("""
+# Two Number Divide App
+This app divides two number
+""")
+#Get Input
+
+st.header('User Input')
+
+def user_input_features():
+    number_1 = st.number_input("Number_1")
+    number_2 = st.number_input("Number_2")
+    
+    data = {'Number_1': number_1,
+            'Number_2': number_2
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+
+for col in df.columns:
+    if df[col].dtype != 'float64':
+        df[col] = df[col].values.astype('float64')
+
+st.table(df)
+
+Result=df.number_1/df.number_2
+
+#Model Inferencing
+
+st.subheader('Result:')
+
+if (df.number_2 != 0):
+    Result=df.number_1/df.number_2
+    st.write(Result)
+else:
+    st.write("Second Number is zero!, hence not divisible.")
